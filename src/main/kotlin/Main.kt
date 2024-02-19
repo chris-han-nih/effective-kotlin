6b9dec6f010bdd2c1632acd5e8471d8be215aeb7
chris.han <namilhan@gmail.com>
2024-02-19 23:17:34 +0900

    check, require, assert function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,8 @@
 package org.example
 
 fun main() {
-//  pop(0)
-  val one = pop(50)
-  val two = pop(100)
+  val one = pop(-50)
+  val two = pop(200)
   val three = pop(10)
   println("one: $one, two: $two, three: $three")
 }
@@ -11,6 +10,6 @@ fun main() {
 fun pop(num: Int = 1): Int {
   require(num >= 0) { "num should be non-negative, but was $num" }
   check(num <= 100) { "num should be less than or equal to 100, but was $num" }
-  assert(num < 50) { "num should be 50, but was $num" }
+  assert(num < 0) { "num should be 50, but was $num" }
   return num + 1
 }
