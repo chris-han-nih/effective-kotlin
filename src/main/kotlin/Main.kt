e0297a012ab246c24d883da27961394b976a8c64
chris.han <namilhan@gmail.com>
2024-02-19 21:52:48 +0900

    update .gitignore

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,16 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-  val name = "Kotlin"
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  println("Hello, " + name + "!")
-  
-  for (i in 1..5) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    println("i = $i")
-  }
+//  pop(0)
+  val one = pop(50)
+  val two = pop(100)
+  val three = pop(10)
+  println("one: $one, two: $two, three: $three")
+}
+
+fun pop(num: Int = 1): Int {
+  require(num >= 0) { "num should be non-negative, but was $num" }
+  check(num <= 100) { "num should be less than or equal to 100, but was $num" }
+  assert(num < 50) { "num should be 50, but was $num" }
+  return num + 1
 }
