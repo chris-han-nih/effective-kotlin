046c721233c619abd81b7e1e34bfb06634b0a21c
chris.han <namilhan@gmail.com>
2024-02-19 23:25:51 +0900

    require를 사용한 nullability 및 스마트 캐스팅

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,15 @@
 package org.example
 
+import java.time.LocalDateTime
+
 fun main() {
-  val one = pop(-50)
+  val one = pop(50)
   val two = pop(200)
   val three = pop(10)
   println("one: $one, two: $two, three: $three")
+  
+  val date = getDate(LocalDateTime.now())
+  println("date: $date")
 }
 
 fun pop(num: Int = 1): Int {
@@ -13,3 +18,9 @@ fun pop(num: Int = 1): Int {
   assert(num < 0) { "num should be 50, but was $num" }
   return num + 1
 }
+
+fun getDate(date: LocalDateTime?): LocalDateTime {
+  require(date is LocalDateTime) { "date should be of type LocalDateTime, but was ${date?.javaClass?.simpleName}" }
+  require(date > LocalDateTime.now()) { "date should be in the future, but was $date" }
+  return date
+ }
