b79f26c353166c6548e89cc8d62f310878d737e0
chris.han <namilhan@gmail.com>
2024-03-27 00:13:29 +0900

    null을 안전하게 처리하기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,12 @@
 package org.example
 
-import java.time.LocalDateTime
-
 fun main() {
-  val one = pop(50)
-  val two = pop(200)
-  val three = pop(10)
-  println("one: $one, two: $two, three: $three")
-  
-  val date = getDate(LocalDateTime.now())
-  println("date: $date")
+  val name = nullable("John")
+  val emptyName = nullable(null)
+  println("name: $name, emptyName: $emptyName")
 }
 
-fun pop(num: Int = 1): Int {
-  require(num >= 0) { "num should be non-negative, but was $num" }
-  check(num <= 100) { "num should be less than or equal to 100, but was $num" }
-  assert(num < 0) { "num should be 50, but was $num" }
-  return num + 1
+fun nullable(name: String?): String {
+  return name ?: "Unnamed"
 }
 
-fun getDate(date: LocalDateTime?): LocalDateTime {
-  require(date is LocalDateTime) { "date should be of type LocalDateTime, but was ${date?.javaClass?.simpleName}" }
-  require(date > LocalDateTime.now()) { "date should be in the future, but was $date" }
-  return date
- }
